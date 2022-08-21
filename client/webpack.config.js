c0b54a30b2deb9c415014420ca0d2321a2dc858c
r-alo <email@example.com>
2022-08-20 21:15:38 -0500

    setup webpack configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,96 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin"); 
+const WebpackPwaManifest = require("webpack-pwa-manifest"); 
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+module.exports = {
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "src/js/index.js"),
+    install: path.resolve(__dirname, "src/js/install.js"),
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "assets/[ext][query]",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
     },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
+    open: true,
+    compress: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-transform-runtime",
+            ],
+          },
+        },
+      },
     ],
-
-    module: {
-      rules: [
-        
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new InjectManifest({
+      swSrc: path.resolve(__dirname, "src/sw.js"),
+      swDest: "srcSw.js",
+    }),
+    new WebpackPwaManifest({
+      fingerprints: true,
+      inject: true,
+      name: "J.A.T.E Progressive Web Application",
+      short_name: "text-editor",
+      description: "simple text editor",
+      background_color: "#236cc5",
+      theme_color: "#236cc5",
+      start_url: "/",
+      publicPath: "/",
+      icons: [
+        {
+          src: path.resolve(__dirname, "src/assets/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
       ],
-    },
-  };
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+      }),
+    ],
+  },
 };
